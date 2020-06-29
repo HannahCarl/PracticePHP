914e045f4dd223579152b2a7168989155d9de89b
Hannah Carl <hcarl@boomtownroi.com>
2020-06-29 08:50:30 -0400

    Update to parse JSON

--- boomtownAssignment.php
+++ boomtownAssignment.php
@@ -15,62 +15,34 @@ $context = stream_context_create(
 );
 
 // Get information from API
+// if internest access is available use the below line, otherwise use the downloaded jsons
 // $file = file_get_contents("https://api.github.com/orgs/boomtownroi", false, $context);
-$apiContents = 
-  '"login": "BoomTownROI",
-  "id": 1214096,
-  "node_id": "MDEyOk9yZ2FuaXphdGlvbjEyMTQwOTY=",
-  "url": "https://api.github.com/orgs/BoomTownROI",
-  "repos_url": "https://api.github.com/orgs/BoomTownROI/repos",
-  "events_url": "https://api.github.com/orgs/BoomTownROI/events",
-  "hooks_url": "https://api.github.com/orgs/BoomTownROI/hooks",
-  "issues_url": "https://api.github.com/orgs/BoomTownROI/issues",
-  "members_url": "https://api.github.com/orgs/BoomTownROI/members{/member}",
-  "public_members_url": "https://api.github.com/orgs/BoomTownROI/public_members{/member}",
-  "avatar_url": "https://avatars3.githubusercontent.com/u/1214096?v=4",
-  "description": "",
-  "name": "BoomTownROI",
-  "company": null,
-  "blog": "boomtownroi.com",
-  "location": null,
-  "email": null,
-  "twitter_username": null,
-  "is_verified": false,
-  "has_organization_projects": true,
-  "has_repository_projects": true,
-  "public_repos": 41,
-  "public_gists": 0,
-  "followers": 0,
-  "following": 0,
-  "html_url": "https://github.com/BoomTownROI",
-  "created_at": "2011-11-22T21:48:43Z",
-  "updated_at": "2020-04-21T23:30:09Z",
-  "type": "Organization"';
-$apiContents = str_replace('"', '', $apiContents);
-$separator = array(': ', ',');
-$apiContents = str_replace($separator, '-', $apiContents);
-$apiArray = explode("-",$apiContents);
+$mainFilePath = 'main.json';
+$mainJSON = file_get_contents($mainFilePath);
+$mainArray = json_decode($mainJSON, true);
+//var_dump($mainArray);
 
 // Find url values that include "api.github.com/orgs/BoomTownROI"
 // Parse through any secondary urls and make sure those are included
-foreach ($apiArray as $apiValue) {
-  if (strpos($apiValue, $urlToCompare) !== false) { 
+foreach ($mainArray as $mainValue) {
+  if (strpos($mainValue, $urlToCompare) !== false) { 
     // Clean up secondary url location
-    if(strpos($apiValue, '{') !== false) { 
-      $apiValue = str_replace('}', '', $apiValue);
-      $secondaryURL = explode('{',$apiValue);
+    if(strpos($mainValue, '{') !== false) { 
+      $mainValue = str_replace('}', '', $mainValue);
+      $secondaryURL = explode('{',$mainValue);
       array_push($urlArray, $secondaryURL[0], $urlToCompare . $secondaryURL[1]);
     }
     else{
-      $urlArray[] = $apiValue; 
+      $urlArray[] = $mainValue; 
     }
   }
 };
+//var_dump($urlArray);
 
 // Check the status of a request for each URL
-foreach ($urlArray as $urlToVisit) {
+//foreach ($urlArray as $urlToVisit) {
   //echo $urlToVisit . "\n";
-  $headerArray = get_headers($urlToVisit, 1, $context);
+  /* $headerArray = get_headers($urlToVisit, 1, $context);
   if(strpos($headerArray[0], '200') === false) { 
     echo "Failed request for " . $urlToVisit . $headerArray[0] . "\n";
   }
@@ -83,6 +55,21 @@ foreach ($urlArray as $urlToVisit) {
       }
       if($responseArray[$x] === 'id'){
         echo "id: " . $responseArray[$x + 1] . "\n";
+      }
+    }
+  } */
+//};
+
+$jsonFileDir = array_diff(scandir('sub_json_files/'), array('..', '.'));
+foreach($jsonFileDir as $subJSONFile) {
+  $all_ids = array();
+  $subJSON = file_get_contents('sub_json_files/' . $subJSONFile);
+  $subJSONObj = json_decode($subJSON);
+  echo "File name: " . $subJSONFile . "\n";
+  foreach($subJSONObj as $row) {
+    foreach($row as $key => $val) {
+      if($key === "id"){
+        echo $key . ': ' . $val . "\n";
       }
     }
   }
@@ -123,6 +110,6 @@ function verifyReposCount($publicReposCount, $reposURL, $context){
   }
 };
 
-verifyDate("2011-11-22T21:48:43Z","2020-04-21T23:30:09Z");
-verifyReposCount(41,"https://api.github.com/orgs/BoomTownROI/repos", $context);
+//verifyDate("2011-11-22T21:48:43Z","2020-04-21T23:30:09Z");
+//verifyReposCount(41,"https://api.github.com/orgs/BoomTownROI/repos", $context);
 ?>
